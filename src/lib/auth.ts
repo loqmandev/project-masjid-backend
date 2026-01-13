3aac055c564eb0d074272e082f615618a4776fef
loqman <loqmanhakim74@gmail.com>
2026-01-13 23:37:11 +0800

    updated mobile scheme

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,7 +11,7 @@ export const auth = betterAuth({
         schema: schema,
     }),
     trustedOrigins: [
-        "projectmasjidmobile://",
+        "jejakmasjidmobile://",
         "exp://",
         "exp://**",
     ],
